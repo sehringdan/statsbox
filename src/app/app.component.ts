272b4d22f6f450724d8c2669d47611c599b91903
Daniel Sehring <daniel.sehring@tui.com>
2019-05-08 14:36:21 +0200

    Creating App

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {hasLifecycleHook} from "@angular/compiler/src/lifecycle_reflector";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'statsbox';
+}
+
